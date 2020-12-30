124f69a12341abbcae27cae17b3d0b51ac2b544d
Luisa Gonzalez Vasquez <lvasquez@client-10-228-239-24.tamulink.tamu.edu>
2020-12-30 12:26:11 -0600

    email

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,4 @@
-function validateEmail(email) {
+/* function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
@@ -48,4 +48,50 @@ $("#contactForm").on("submit", function (event) {
       $("#contactForm [type=submit]").text("Send");
     }, 4000);
   });
-}); //end form submit handler
+}); //end form submit handler */
+
+window.addEventListener("DOMContentLoaded", function() {
+		  
+  // get the form elements defined in your form HTML above
+  
+  var form = document.getElementById("contactForm");
+  var button = document.getElementById("my-form-button");
+  var status = document.getElementById("my-form-status");
+
+  // Success and Error functions for after the form is submitted
+  
+  function success() {
+  form.reset();
+  button.style = "display: none ";
+  status.innerHTML = "Thanks!";
+  }
+
+  function error() {
+  status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function(ev) {
+  ev.preventDefault();
+  var data = new FormData(form);
+  ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+  if (xhr.status === 200) {
+    success(xhr.response, xhr.responseType);
+  } else {
+    error(xhr.status, xhr.response, xhr.responseType);
+  }
+  };
+  xhr.send(data);
+}
